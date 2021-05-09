5a855fa3da0443a45a8c9b01fcf545a183f5a2d3
Conrad S <conrad.schloer@gmail.com>
2021-05-09 15:38:33 +0200

    Bump to small

--- bcodmo_sm/ec2.tf
+++ bcodmo_sm/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami                         = var.instance_ami
   availability_zone           = "us-east-1a"
-  instance_type               = "t3.micro"
+  instance_type               = "t3.small"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sm_ui.id]
   key_name = "sm_ui"
@@ -12,3 +12,7 @@ resource "aws_instance" "instance" {
  
 }
 
+output "ec2_public_dns" {
+  value       = aws_instance.instance.public_dns
+  description = "The DNS of the SM UI application ec2 instance"
+}
