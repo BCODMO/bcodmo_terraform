844bbb0f2b00876dc182e5478f12dd372739f81a
Conrad S <conrad.schloer@gmail.com>
2022-01-24 12:11:20 +0100

    Fix EFS security group

--- laminar/efs.tf
+++ laminar/efs.tf
@@ -9,10 +9,10 @@ resource "aws_efs_file_system" "efs_volume" {
 resource "aws_efs_mount_target" "laminar_cache1b" {
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = aws_default_subnet.default_1b.id
-  security_groups = [aws_security_group.laminar.id]
+  security_groups = [aws_security_group.laminar_hidden.id]
 }
 resource "aws_efs_mount_target" "laminar_cache1a" {
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = aws_default_subnet.default_1a.id
-  security_groups = [aws_security_group.laminar.id]
+  security_groups = [aws_security_group.laminar_hidden.id]
 }
