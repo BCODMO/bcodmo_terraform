6911ada19c070ee7f89a935d3f06774147b97bd9
Conrad S <conrad.schloer@gmail.com>
2022-01-25 12:42:36 +0100

    Add access policy for laminar secrets

--- laminar/ecs.tf
+++ laminar/ecs.tf
@@ -2,6 +2,31 @@ resource "aws_ecs_cluster" "laminar" {
   name = "laminar-${terraform.workspace}"
 }
 
+resource "aws_iam_role_policy" "secrets_access_policy" {
+  name = "laminar_secrets_access_policy_${terraform.workspace}"
+  role = aws_iam_role.ecs_role.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+            "secretsmanager:GetResourcePolicy",
+            "secretsmanager:GetSecretValue",
+            "secretsmanager:DescribeSecret",
+            "secretsmanager:ListSecretVersionIds"
+        ],
+        "Effect": "Allow",
+        "Resource": [
+            "${aws_secretsmanager_secret.github_access_token.arn}"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
 resource "aws_iam_role_policy" "efs_access_policy" {
   name = "laminar_efs_access_policy_${terraform.workspace}"
   role = aws_iam_role.ecs_role.id
