1192bbcba14377b2809233b8d56ec55eb17faa44
Conrad S <conrad.schloer@gmail.com>
2022-01-31 12:34:42 +0100

    Fix bug with security groups

--- laminar/vpc.tf
+++ laminar/vpc.tf
@@ -68,4 +68,14 @@ resource "aws_security_group" "laminar_hidden" {
   }
 }
 
+resource "aws_security_group_rule" "allow_laminar_hidden" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.laminar_hidden.id
+  security_group_id        = aws_security_group.laminar.id
+
+}
+
 
