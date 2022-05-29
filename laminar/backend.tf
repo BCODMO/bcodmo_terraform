b2142577f0d1ac439ff92cd462ab02ae2475aab0
Conrad S <conrad.schloer@gmail.com>
2022-05-29 12:49:26 +0200

    Update scripts to reflect new terraform version

--- laminar/backend.tf
+++ laminar/backend.tf
@@ -4,20 +4,25 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "bcodmo-laminar-terraform-state"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  # Enable server-side encryption by default
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
   # Enable versioning so we can see the full revision history of our
   # state files
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
--- laminar/s3.tf
+++ laminar/s3.tf
@@ -1,7 +1,97 @@
-
 resource "aws_s3_bucket" "results" {
   bucket = "laminar-results"
+}
+
+resource "aws_s3_bucket_acl" "results" {
+  bucket = aws_s3_bucket.results.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_cors_configuration" "results" {
+  bucket = aws_s3_bucket.results.id
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "results" {
+  bucket = aws_s3_bucket.results.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "results" {
+  bucket = aws_s3_bucket.results.id
+  rule {
+    id      = "expire_objects"
+    filter {}
+    expiration {
+      days = 20
+    }
+    status = "Enabled"
+  }
+}
+
+
+
+resource "aws_s3_bucket" "load" {
+  bucket = "laminar-load"
+}
+resource "aws_s3_bucket_acl" "load" {
+  bucket = aws_s3_bucket.load.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_public_access_block" "load" {
+  bucket = aws_s3_bucket.load.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket" "history" {
+  bucket = "laminar-history"
+}
+
+resource "aws_s3_bucket_acl" "history" {
+  bucket = aws_s3_bucket.history.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_public_access_block" "history" {
+  bucket = aws_s3_bucket.history.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket" "dump" {
+  bucket = "laminar-dump"
+}
+resource "aws_s3_bucket_acl" "dump" {
+  bucket = aws_s3_bucket.dump.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_public_access_block" "dump" {
+  bucket = aws_s3_bucket.dump.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+resource "aws_s3_bucket_cors_configuration" "dump" {
+  bucket = aws_s3_bucket.dump.id
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
@@ -9,29 +99,4 @@ resource "aws_s3_bucket" "results" {
     max_age_seconds = 3000
   }
 
-  lifecycle_rule {
-    id      = "expire_objects"
-    enabled = true
-
-    prefix = "*"
-
-    expiration {
-      days = 20
-    }
-  }
-}
-
-resource "aws_s3_bucket" "load" {
-  bucket = "laminar-load"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "history" {
-  bucket = "laminar-history"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "dump" {
-  bucket = "laminar-dump"
-  acl    = "private"
 }
